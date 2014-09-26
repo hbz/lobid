ee6c24751a0ab693c3a7aeaa76308246e5bdffee
Fabian Steeg <fsteeg@gmail.com>
2014-09-26 18:12:29 +0200

    Update to Play 2.3.4, Scala 2.11.2, and SBT 0.13.6

--- project/Build.scala
+++ project/Build.scala
@@ -1,27 +1,32 @@
 import sbt._
 import Keys._
-import play.Project._
+import play.Play.autoImport._
+import PlayKeys._
 
 object ApplicationBuild extends Build {
-
+  
     val appName         = "lobid"
     val appVersion      = com.typesafe.config.ConfigFactory.parseFile(new File("conf/application.conf")).resolve().getString("application.version")
     val appDependencies = Seq(
       javaCore,
       cache,
-      "com.typesafe.play" % "play-test_2.10" % "2.2.2",
-      "org.elasticsearch" % "elasticsearch" % "1.1.0" withSources(),
+      "com.typesafe.play" % "play-test_2.11" % "2.3.4",
+      "org.elasticsearch" % "elasticsearch" % "1.3.2" withSources(),
       "org.lobid" % "lodmill-ld" % "2.0.1",
-      "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+      "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test",
+      "com.google.gdata" % "core" % "1.47.1"
     )
-
-    val main = play.Project(appName, appVersion, appDependencies).settings(
+    
+    val main = Project(appName, file(".")).enablePlugins(play.PlayJava).settings(
+      scalaVersion := "2.11.2",
       parallelExecution in Test := false,
       resolvers := Seq(
           "codehaus" at "http://repository.codehaus.org/org/codehaus", 
           "typesafe" at "http://repo.typesafe.com/typesafe/repo", 
           "jena-dev" at "https://repository.apache.org/content/repositories/snapshots",
-          Resolver.mavenLocal)
+          Resolver.mavenLocal),
+      version := appVersion,
+      libraryDependencies ++= appDependencies
     )
 
     val javacOptions = Seq("-source", "1.8", "-target", "1.8")
