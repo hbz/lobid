051fd7b2fcb874855d9ed190b8a6823335e1953a
pascal Christoph <christoph@hbz-nrw.de>
2015-01-21 14:47:57 +0100

    Use http api instead of trying to connect to an ES node directly
    As said in the commit before - lookups to get the data are
    to be made using the api exposed using http.

--- test/tests/RenewTestData.java
+++ test/tests/RenewTestData.java
@@ -5,22 +5,15 @@ package tests;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 
-import models.Index;
-import models.Parameter;
-import models.Search;
-
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.index.query.QueryBuilder;
+import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-import com.google.common.collect.ImmutableMap;
+import com.github.jsonldjava.core.JsonLdOptions;
 
 /**
  * Rebuilds the test data by making a lookup at the lobid api to get the
@@ -36,12 +29,14 @@ public abstract class RenewTestData {
 			"test/tests/json-ld-index-data-to-build.json";
 	private static StringBuilder lookupedJson = new StringBuilder();
 	static boolean finished = false;
+	static JsonLdOptions options = new JsonLdOptions(
+			"http://api.lobid.org/context/gnd.json");
 
 	public static void main() throws IOException {
 		System.out.println("Start getting test data ...");
 		File sampleData = new File(TEST_DATA);
 		try (Scanner scanner = new Scanner(sampleData)) {
-			runRequests(scanner, Search.client);
+			runRequests(scanner);
 			File testFile = new File("test/tests/json-ld-index-data.json");
 			try (FileOutputStream fos = new FileOutputStream(testFile)) {
 				if (lookupedJson.length() > 1) {
@@ -54,45 +49,47 @@ public abstract class RenewTestData {
 		}
 	}
 
-	private static void runRequests(final Scanner scanner, final Client c) {
+	private static void runRequests(final Scanner scanner) {
 		while (scanner.hasNextLine()) {
 			final String meta = scanner.nextLine();
-			addIndexRequest(meta, c);
+			addIndexRequest(meta);
 
 		}
 	}
 
-	private static void addIndexRequest(final String meta, final Client c) {
+	private static void addIndexRequest(final String meta) {
 		System.out.println(meta);
-		final String hit = createRequestBuilder(meta, c);
+		final String hit = lookupData(meta);
 		if (hit == null) {
 			System.err.println("No results. Make sure resource exits.");
 		} else {
 			lookupedJson.append(meta + "\n" + hit + "\n");
 		}
-
 	}
 
-	private static String createRequestBuilder(final String meta, Client c) {
+	private static String lookupData(final String meta) {
 		String ret = null;
-		final JSONObject object =
+		JSONObject object =
 				(JSONObject) ((JSONObject) JSONValue.parse(meta)).get("index");
-		final String index = (String) object.get("_index");
-		final String type = (String) object.get("_type");
-		final String id = (String) object.get("_id"); // NOPMD
-		final Map<Parameter, String> parameters =/*@formatter:off*/
-				Parameter.select(new ImmutableMap.Builder<Parameter, String>()
-						.put(Parameter.ID, id).build());/*@formatter:on*/
-		Search search = new Search(parameters, Index.id(index));
-		final QueryBuilder queryBuilder = search.createQuery();
-		SearchRequestBuilder requestBuilder =
-				c.prepareSearch(index).setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-						.setQuery(queryBuilder).setIndices(index).setTypes(type);
-		final SearchResponse response =
-				requestBuilder.setFrom(0).setSize(1).setExplain(false).execute()
-						.actionGet();
-		if (response.getHits().getHits().length != 0)
-			ret = response.getHits().getAt(0).getSourceAsString();
+		String id = ((String) object.get("_id"));
+		id =
+				id.replaceAll("http:\\/\\/d-nb.info/gnd\\/",
+						"http:\\/\\/lobid.org\\/subject/");
+		int i = id.lastIndexOf("/");
+		id =
+				id.substring(0, i).concat("?id=").concat(id.substring(i + 1))
+						.concat("&format=internal");
+		URL url;
+		try {
+			url = new URL(id);
+			HttpURLConnection urlConnection =
+					(HttpURLConnection) url.openConnection();
+			urlConnection.setRequestProperty("Accept", "application/ld+json");
+			urlConnection.connect();
+			ret = IOUtils.toString(urlConnection.getInputStream(), "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return ret;
 
 	}
