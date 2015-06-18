hbz/lobid

commit ba66ee2f65ff273182cfd67641642d9dc8d1474a

Remove statementOfResponsibility from author search.
See #129.

app/models/queries/AbstractIndexQuery.java
@@ -47,7 +47,7 @@ public abstract class AbstractIndexQuery {
 			final String term =
 					search.startsWith("http") ? search : "http://d-nb.info/gnd/" + search;
 			query =
-					multiMatchQuery(term, fields().subList(4, fields().toArray().length)
+					multiMatchQuery(term, fields().subList(3, fields().toArray().length)
 							.toArray(new String[] {}));
 		} else {
 			query = nameMatchQuery(search);

app/models/queries/LobidResources.java
@@ -97,10 +97,9 @@ public class LobidResources {
 		public List<String> fields() {
 			return Arrays
 					.asList(
-							"@graph.http://rdvocab.info/Elements/statementOfResponsibility.@value",
+                            "@graph.http://purl.org/dc/elements/1.1/contributor.@value",
 							"@graph.http://d-nb.info/standards/elementset/gnd#dateOfBirth.@value",
 							"@graph.http://d-nb.info/standards/elementset/gnd#dateOfDeath.@value",
-							"@graph.http://purl.org/dc/elements/1.1/contributor.@value",
 							"@graph.http://purl.org/dc/terms/creator.@id",
 							"@graph.http://purl.org/dc/terms/contributor.@id",
 							"@graph.http://id.loc.gov/vocabulary/relators/act.@id",

commit 5f6bb4efa1de2ce52908d4fb89056c51cd900756

Fix searching author
See #163.

Bug introduced with ba66ee2f65ff273182cfd67641642d9dc8d1474a.
So one value field would be also queried, amongst the id fields.

Increasing the list parameter to start at '4' only id fields are queried.

app/models/queries/AbstractIndexQuery.java
@@ -47,7 +47,7 @@ public abstract class AbstractIndexQuery {
 			final String term =
 					search.startsWith("http") ? search : "http://d-nb.info/gnd/" + search;
 			query =
-					multiMatchQuery(term, fields().subList(3, fields().toArray().length)
+					multiMatchQuery(term, fields().subList(4, fields().toArray().length)
 							.toArray(new String[] {}));
 		} else {
 			query = nameMatchQuery(search);