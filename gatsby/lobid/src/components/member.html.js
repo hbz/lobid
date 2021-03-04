2b04679701ed89aed5e38558d134d7a9147b6fda
Fabian Steeg <steeg@hbz-nrw.de>
2021-03-04 17:30:00 +0100

    Tweak presentation of `publication.about` data
    See https://github.com/hbz/lobid/issues/445

--- gatsby/lobid/src/components/member.html.js
+++ gatsby/lobid/src/components/member.html.js
@@ -71,14 +71,16 @@ export class Member extends React.Component {
                 <p className="lead">{this.props.publications}</p>
                 <table className="table table-striped table-condensed">
                   <thead>
-                    <tr><th width="10%" /><th width="70%" /><th width="10%" /><th width="10%" /></tr>
+                    <tr><th width="10%" /><th width="65%" /><th width="12%" /><th width="10%" /><th width="3%" /></tr>
                   </thead>
                   <tbody>
                     {this.props.pubs.map(publication =>
                       <tr>
                         <td><small>{publication.datePublished}</small></td>
                         <td><a href={publication.id}>{publication.name.de || publication.name.en || publication.id}</a></td>
-                        <td>{publication.about && publication.about.map(a => <p><a href={a.id}>{a.id}</a></p>)}</td>
+                        <td>{publication.about && publication.about.map(a =>
+                          <p><small><span class="glyphicon glyphicon-tag" aria-hidden="true"></span></small>&nbsp;<a href={a.id}>{a.id.split("://")[1].split(".")[0]}</a></p>
+                        )}</td>
                         <td align="right"><small><a href={"https://schema.org/" + publication.type}>{publication.type}</a></small></td>
                         <td><a title="Beschreibung als JSON-LD anzeigen" href={publication.fields.jsonFile}><img height="20px" src={jsonLdPng} alt="JSON-LD" /></a></td>
                       </tr>
