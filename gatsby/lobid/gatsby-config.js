148e8fcf80839340a93234a9a83d7fde859c269c
Fabian Steeg <steeg@hbz-nrw.de>
2022-06-27 15:05:10 +0200

    Add basic team RSS feed based on `gatsby-plugin-feed`
    See https://github.com/hbz/slides/issues/81

--- gatsby/lobid/gatsby-config.js
+++ gatsby/lobid/gatsby-config.js
@@ -6,6 +6,61 @@
 
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+            }
+          }
+        }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allPublicationJson } }) => {
+              return allPublicationJson.edges.map(edge => {
+                return Object.assign({}, edge.node.id, {
+                  description: edge.node.description.de || edge.node.description.en,
+                  title: edge.node.name.de || edge.node.name.en,
+                  date: edge.node.datePublished,
+                  url: edge.node.id,
+                  guid: edge.node.id,
+                })
+              })
+            },
+            query: `
+              {
+                allPublicationJson(
+                  sort: { order: DESC, fields: [datePublished] },
+                ) {
+                  edges {
+                    node {
+                      id
+                      name {
+                        de
+                        en
+                      }
+                      description {
+                        de
+                        en
+                      }
+                      datePublished
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/team/rss.xml",
+            title: "Lobid team RSS feed",
+            description: "Publications and presentation by the Lobid team",
+          },
+        ],
+      },
+    },
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
