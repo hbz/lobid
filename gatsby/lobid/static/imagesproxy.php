e51e29e715022c4b07730f5c961c0f1d9d59f6bd
Phu Tu <phu.tu@hbz-nrw.de>
2025-11-17 12:45:16 +0100

    Removes trailing slash from domain

--- gatsby/lobid/static/imagesproxy.php
+++ gatsby/lobid/static/imagesproxy.php
@@ -20,7 +20,7 @@
         'www.deutsche-biographie.de',
         'www.archivportal-d.de',
         'kalliope-verbund.info',
-        'www.portraitindex.de/',
+        'www.portraitindex.de',
         'lobid.org'
     ];
     $parsedUrl = parse_url($url);
